3f72815e0c2c806f3ba38cff12e131d8584f6599
marinho <diogomarinhomg@gmail.com>
2021-10-24 23:16:13 -0300

    Update Main.java

--- PrimeiroProjeto/src/pacote01/Main.java
+++ PrimeiroProjeto/src/pacote01/Main.java
@@ -3,18 +3,6 @@ package pacote01;
 import javax.swing.JOptionPane;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        String name = JOptionPane.showInputDialog("Enter your name");
-        JOptionPane.showMessageDialog(null, "Hello " + name);
-
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-        JOptionPane.showMessageDialog(null, "You are" +height+ "yaers old");
-
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-        JOptionPane.showMessageDialog(null, "You are" +height+ "cm tall");
-    
-    }
-    
-}
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    } 
