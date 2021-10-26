01246898d1d73f81b14353fbc2ed775c78f0b2fa
marinho <diogomarinhomg@gmail.com>
2021-10-25 22:52:34 -0300

    Update Main.java

--- PrimeiroProjeto/src/pacote01/Main.java
+++ PrimeiroProjeto/src/pacote01/Main.java
@@ -8,6 +8,10 @@ public class Main {
         String name = JOptionPane.showInputDialog("Enter your name");
         JOptionPane.showMessageDialog(null, ("Hello  " +name));
 
-        int age = JOptionPane.showInputDialog("Enter your age");
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are  " +age+ " years your old");
+
+        Double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "You are  " +height+ " cm tall");
     } 
 }
