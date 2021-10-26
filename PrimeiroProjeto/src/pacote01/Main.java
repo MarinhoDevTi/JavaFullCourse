8090c538dc96bbec8c79c48825be91d660b88b88
marinho <diogomarinhomg@gmail.com>
2021-10-25 21:43:19 -0300

    Update Main.java

--- PrimeiroProjeto/src/pacote01/Main.java
+++ PrimeiroProjeto/src/pacote01/Main.java
@@ -5,4 +5,5 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.printls("Conhecendo a Programação Java")
     } 
