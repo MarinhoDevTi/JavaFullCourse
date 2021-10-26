3249219ba333dbd7f7a4ba3838a91babd0558c9f
marinho <diogomarinhomg@gmail.com>
2021-10-25 22:03:41 -0300

    Update Main.java

--- PrimeiroProjeto/src/pacote01/Main.java
+++ PrimeiroProjeto/src/pacote01/Main.java
@@ -3,7 +3,9 @@ package pacote01;
 import javax.swing.JOptionPane;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.printls("Conhecendo a Programação Java")
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name");
+        
     } 
+}
