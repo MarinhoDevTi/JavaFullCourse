b0fa3fcc56332452ba5789583cba536548138c62
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-14 20:43:01 -0300

    Update

---
+++ interfaceGrafica/src/ClassePrincipal.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class ClassePrincipal {
+    public static void main(String[] args) {
+
+        String nome = JOptionPane.showInputDialog("digite seu Nome: ");
+        JOptionPane.showMessageDialog(null, "Hello" + nome);
+
+        
+    }
+}
--- variaveis/src/ClassePrincipal.java
+++ variaveis/src/ClassePrincipal.java
@@ -1,5 +1,13 @@
 public class ClassePrincipal {
     public static void main(String[] args) {
         
+        //declaração de variaveis
+        int x;
+
+        x = 123;
+
+        System.out.println("Este é o valor de x " + x);
+
+        
     }
 }
