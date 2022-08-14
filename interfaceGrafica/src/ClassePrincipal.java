434f44836d4f471fd10a0d8a207fb311dfe3e1bf
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-14 20:46:18 -0300

    Update ClassePrincipal.java

--- interfaceGrafica/src/ClassePrincipal.java
+++ interfaceGrafica/src/ClassePrincipal.java
@@ -4,7 +4,7 @@ public class ClassePrincipal {
     public static void main(String[] args) {
 
         String nome = JOptionPane.showInputDialog("digite seu Nome: ");
-        JOptionPane.showMessageDialog(null, "Hello" + nome);
+        JOptionPane.showMessageDialog(null, "Hello " + nome);
 
         
     }
