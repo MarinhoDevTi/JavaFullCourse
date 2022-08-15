0ab46f783b010e33503189afb85ab3d5aa20048d
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-14 21:14:52 -0300

    Update ClassePrincipal.java

--- interfaceGrafica/src/ClassePrincipal.java
+++ interfaceGrafica/src/ClassePrincipal.java
@@ -6,6 +6,10 @@ public class ClassePrincipal {
         String nome = JOptionPane.showInputDialog("digite seu Nome: ");
         JOptionPane.showMessageDialog(null, "Hello " + nome);
 
+        int ano = Integer.parseInt(JOptionPane.showInputDialog("Entre com seu ano "));
+        JOptionPane.showMessageDialog(null, "Você nasceu em :  " + ano); 
+
+
         
     }
 }
