6422fa4d92d0e1e134a50af7bdd8fbcf843dc206
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-09-19 00:00:07 -0300

    Update

---
+++ OperadoresLogicos/src/ClassePrincipal.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ClassePrincipal {
+    public static void main(String[] args) {
+        //variavel de leitura 
+        Scanner leia = new Scanner(System.in);
+
+        
+    }
+}
