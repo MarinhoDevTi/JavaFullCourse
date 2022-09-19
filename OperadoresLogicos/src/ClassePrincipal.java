6daf652e986e57fc29dbba5b14a17e0eed2f8247
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-09-19 07:48:16 -0300

    Update

--- OperadoresLogicos/src/ClassePrincipal.java
+++ OperadoresLogicos/src/ClassePrincipal.java
@@ -5,6 +5,13 @@ public class ClassePrincipal {
         //variavel de leitura 
         Scanner leia = new Scanner(System.in);
 
-        
+        System.out.println("You are playing a game ! Press q or Q to quit");;
+        String response = leia.next();
+
+        if(response.equals("q") || response.equals("Q")){
+            System.out.println("You quit the game");
+        } else {
+            System.out.println("You are still playing the game *pew pew*");
+        }
     }
 }
